147a09490e93730714bc3f4f2268fbf217233d26
ThiagoRampim <thiago.rodriguesrampim@gmail.com>
2022-09-24 15:27:24 -0300

    Change security constants to const val

--- src/main/kotlin/com/kanban/board/domain/constant/SecurityConstants.kt
+++ src/main/kotlin/com/kanban/board/domain/constant/SecurityConstants.kt
@@ -1,8 +1,8 @@
 package com.kanban.board.domain.constant
 
 object SecurityConstants {
-    val SECRET = "lBz64DHasAXfjNhK3YmRIrC5jHAiF3AA"
-    val EXPIRATION_TIME = 900_000 // 15 minutes in milliseconds
-    val TOKEN_PREFIX = "Bearer "
-    val HEADER_STRING = "Authorization"
+    const val SECRET = "lBz64DHasAXfjNhK3YmRIrC5jHAiF3AA"
+    const val EXPIRATION_TIME = 900_000 // 15 minutes in milliseconds
+    const val TOKEN_PREFIX = "Bearer "
+    const val HEADER_STRING = "Authorization"
 }
